2aa670c8ba40c5b41cdf18dbe11e7efefd17501c
jimmynetor3 <54798823+jimmynetor3@users.noreply.github.com>
2020-07-23 00:56:02 +0200

    af

--- openssl-encrypt.php
+++ openssl-encrypt.php
@@ -9,17 +9,17 @@ $key = hash("sha256", $not_so_secret_key);
 
 $iv = substr(hash("sha256", $not_so_secret_IV), 0, 32);
 
-$output_encrypted = openssl_encrypt($input, $method, $key, 0,$iv,$tag);
+$output_encrypted = openssl_encrypt($input, $method, $key, 0, $iv, $tag);
 $output_encrypted = base64_encode($output_encrypted);
-echo "de encrypted data is " .$output_encrypted . "\n";
+echo "de encrypted data is " . $output_encrypted . "\n";
 
 
-$output_decrypted = openssl_decrypt(base64_decode($output_encrypted), $method,$key,0,$iv,$tag );
+$output_decrypted = openssl_decrypt(base64_decode($output_encrypted), $method, $key, 0, $iv, $tag);
 
 echo "de decrypted data is $output_decrypted \n";
 
-if ($input === $output_decrypted){
+if ($input === $output_decrypted) {
     echo "succes";
-} else{
+} else {
     echo 'mislukt';
 }
